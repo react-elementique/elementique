72115239bc19ad2dff7314992b0f6c9ec5078ac3
unknown <shiveshtiwari0@gmail.com>
2024-05-28 05:31:50 +0530

    setup publish to npm to github actions

--- src/components/input/__test__/Input.test.tsx
+++ src/components/input/__test__/Input.test.tsx
@@ -6,7 +6,7 @@ import Input from "../Input";
 describe("Input component", () => {
     it("should render Input component correctly", () => {
         render(<Input />);
-        const el = screen.getByRole("textbox");
-        expect(el).toBeInTheDocument();
+        const element = screen.getByRole("textbox");
+        expect(element).toBeInTheDocument();
     });
 });
